45685caef8d16cc5e90f0f9394bfa822770ad0bc
Mait Roosvalt <maitzzzz12@gmail.com>
2016-05-16 22:09:38 +0300

    update

--- app/src/main/java/com/example/maitroosvalt/calcreceiver/CalcReceiver.java
+++ app/src/main/java/com/example/maitroosvalt/calcreceiver/CalcReceiver.java
@@ -13,46 +13,63 @@ import java.math.RoundingMode;
 
 
 public class CalcReceiver extends BroadcastReceiver {
+    Calculator calc = new Calculator();
+    public CalcReceiver() {
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (isOrderedBroadcast()) {
             Bundle extras = intent.getExtras();
+
             if (extras != null) {
                 UOW uow = new UOW(context);
 
-                Double  num1 = extras.getDouble("nr1");
-                Double  num2 = extras.getDouble("nr2");
-                String op = extras.getString("op");
-                Double answer = calculate(num1, num2, op);
-                setResultCode(Activity.RESULT_OK);
-                setResultData(answer.toString());
-                uow.addNewRowToStats(op, num1, num2, answer);
-            }
+                String button = extras.getString("button");
+                String screen = extras.getString("screen");
 
+                switch (button) {
+                    case "=":
+                        Double nr1  = new Double(screen);
+                        Double nr2 =  new Double(calc.getX());
+                        String op = calc.getOperator();
+
+                        Float answer = OnEquals(screen);
+
+                        uow.addNewRowToStats(op, nr1, nr2,  new Double(answer.toString()));
+
+                        extras.putString("screen_val", Float.toString(answer));
+
+                        setResultExtras(extras);
+
+                        break;
+                    default:
+                        onOperator(button, screen);
+                        extras.putString("screen_val", "");
+                        setResultExtras(extras);
+
+                        break;
+                }
+            }
         }
 
     }
 
-    public static Double calculate(Double d1, Double d2, String op) {
+    public Float OnEquals(String value) {
+        calc.setY(Float.parseFloat(value));
 
-        BigDecimal answer = BigDecimal.ZERO;
-        try {
-            BigDecimal val1 = BigDecimal.valueOf(d1);
-            BigDecimal val2 = BigDecimal.valueOf(d2);
-            if (op.equals("+")) {
-                answer = val1.add(val2);
-            } else if (op.equals("-")) {
-                answer = val1.subtract(val2);
-            } else if (op.equals("*")) {
-                answer = val1.multiply(val2);
-            } else if (op.equals("/")) {
-                answer = val1.divide(val2, 10, RoundingMode.HALF_UP);
-            }
-        } catch (ArithmeticException a) {
-            return Double.NaN;
-        } catch (NumberFormatException n) {
-            return Double.NaN;
-        }
-        return answer.doubleValue();
+        float total = calc.calc(calc.getOperator());
+
+        calc.setY(null);
+        calc.setX(null);
+        calc.setOperator(null);
+
+        return total;
+    }
+
+    public void onOperator(String operator, String value) {
+        calc.setOperator(operator);
+
+        calc.setX(Float.parseFloat(value));
     }
 }
